bc8f79be12d8658e8ac4b29e5c0e2f6cf24b355b
falldj <derek.isensee@gmail.com>
2016-08-29 22:15:35 -0500

    added rest of files
    also added jquery/bootstrap/etc.

--- index.php
+++ index.php
@@ -1,6 +1,27 @@
 <!DOCTYPE html>
 <html>
-    <div>
-        <h1>Hello! My name is! wiki wiki Slim Shady!</h1>
-    </div>
+    <head>
+        <!--Links for font awesome, jquery UI, and bootstrap. also calls the index.css file in the css folder-->
+        <link rel="stylesheet" href="font-awesome-4.6.3/css/font-awesome.min.css">
+        <link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css'>
+        <link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.1/animate.min.css'>
+        <link rel="stylesheet" href="css/index.css">
+        
+        <div id="dominoStatsHead" class="container">
+            <h1>Super Duper Domino Stats</h1>
+        </div>
+    </head>
+    
+    <body>
+        <p>Apparently, a form will go here.</p>
+        <form action="processData.php" method="post">
+            <input type="text" name="thisIsAPlaceholder">
+            <input type="text" name="anotherPlaceholder">
+            <input type="submit">
+        </form>
+        
+        <script src='https://code.jquery.com/jquery-2.2.4.min.js'></script>
+        <script src='https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js'></script>
+        <script src='js/index.js'></script>
+    </body>
 </html>
